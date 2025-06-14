51a2096d76f6df40b1661059e6da377a188f6692
pwoofter <phoebewoofter@gmail.com>
2025-06-13 17:20:47 -0700

    Fixes gradient

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode className="amorphous-gradient">
-    <App />
+  <StrictMode>
+    <App className="amorphous-gradient"/>
   </StrictMode>,
 )
